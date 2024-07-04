1ceb1193774d8e66e0a859440f13f087ca7194dc
Steve Landry Tene <steve.tene@gmail.com>
2024-07-03 21:39:48 -0400

    adding gin framework

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Garage API")
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // Listen and serve on 0.0.0.0:8080
 }
